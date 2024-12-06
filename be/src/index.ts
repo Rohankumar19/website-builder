8f8c902572eceff874dae8f12702cad677965461
Rohan Kumar <rohankumar19003@gmail.com>
2024-12-06 08:55:33 +0530

    added anthropic sdk

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,1 +1,18 @@
 require("dotenv").config();
+
+import { Anthropic } from '@anthropic-ai/sdk';
+
+const CLAUDE_API_KEY = process.env.CLAUDE_API_KEY;
+
+
+const anthropic = new Anthropic();
+
+async function main() {
+const msg = await anthropic.messages.create({
+  model: "claude-3-sonnet-20240229",
+  max_tokens: 1000,
+  temperature: 0,
+  messages: []
+});
+
+console.log(msg);
